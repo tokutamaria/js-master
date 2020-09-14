5e25e0c27832066a634950b5a9251277bc8fcace
Takuto Arima <ta910arima@gmail.com>
2020-09-14 09:56:44 +0900

    チェーンメソッド終了

--- 060_関数とオブジェクト２/080_プロトタイプ継承/start/main.js
+++ 060_関数とオブジェクト２/080_プロトタイプ継承/start/main.js
@@ -6,3 +6,24 @@ function Person(name, age) {
 Person.prototype.hello = function() {
   console.log('hello ' + this.name);
 }
+
+function Japanese(name, age, gender) {
+  Person.call(this,name, age);
+  this.gender = gender;
+}
+
+Japanese.prototype = Object.create(Person.prototype);
+
+Japanese.prototype.hello = function() {
+  console.log('Sayonara' + this.name);
+}
+Japanese.prototype.bye = function() {
+  console.log('AI' + this.name);
+}
+
+const taro = new Japanese('Taro' , 23 ,'Male');
+
+console.log(taro);
+
+taro.hello();
+taro.bye();
--- 060_関数とオブジェクト２/100_クラス継承/start/main.js
+++ 060_関数とオブジェクト２/100_クラス継承/start/main.js
@@ -1,25 +1,33 @@
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+
+  }
+
+  hello() {
+        console.log('hello ' + this.name);
+      }
 }
 
-Person.prototype.hello = function() {
-  console.log('hello ' + this.name);
-}
 
-function Japanese(name, age, gender) {
-  Person.call(this, name, age);
-  this.gender = gender;
-}
-
-Japanese.prototype = Object.create(Person.prototype);
-
-Japanese.prototype.hello = function() {
+class Japanese extends Person {
+  constructor(name, age, gender) {
+    super(name, age);
+    this.gender = gender;
+  }
+  hello() {
   console.log('Konnichiwa ' + this.name);
+  }
+
+  bye() {
+    console.log('Sayonara ' + this.name);
+  }
+
+
 }
 
-Japanese.prototype.bye = function() {
-  console.log('Sayonara ' + this.name);
-}
 
 const taro = new Japanese('Taro', 23, 'Male');
+console.log(taro);
+taro.bye();
